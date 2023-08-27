bb1b4a9701108d8cdc582043a89c226b468077a5
Hiromichi Goto <hirox777@gmail.com>
2023-08-27 13:51:05 +0900

    * Change type of input/output * Add debug printf function

--- bootloader/F1/Src/crypt.cc
+++ bootloader/F1/Src/crypt.cc
@@ -2,28 +2,27 @@
 #include <cstdint>
 #include <cstdio>
 #include <bit>
+#include <string>
 
 #include "crypt.h"
 
-static void dump_plain(const std::uint64_t* in, const std::uint64_t extended_key[2 * ROUNDS]) {
+#if defined(CRYPT_DEBUG)
+static void dump_key(const std::uint64_t extended_key[2 * ROUNDS]) {
     printf("Key: ");
     for (auto x = 0; x < 2 * ROUNDS; x++) {
         printf("0x%08llx ", extended_key[x]);
     }
-    printf("\nPlain: ");
-    for (auto x = 0; x < 10; x++) {
-        printf("0x%08llx ", in[x]);
-    }
     printf("\n");
 }
 
-static void dump_crypt(std::uint64_t* out) {
-    printf("Crypt: ");
+static void dump_data(const std::string& tag, const std::uint64_t* data) {
+    printf("%s: ", tag.c_str());
     for (auto x = 0; x < 10; x++) {
-        printf("0x%08llx ", out[x]);
+        printf("0x%08llx ", data[x]);
     }
     printf("\n");
 }
+#endif
 
 static void speck128_round(std::uint64_t* x, std::uint64_t* y, std::uint64_t k) {
     *x = std::rotr(*x, 8);
@@ -45,15 +44,15 @@ static void decrypt_internal(
         std::uint64_t pt[2],
         const std::uint64_t ct[2],
         const std::uint64_t K[ROUNDS * 2]) {
-   std::uint64_t y = ct[0];
-   std::uint64_t x = ct[1];
+    std::uint64_t y = ct[0];
+    std::uint64_t x = ct[1];
 
-   for (std::int32_t i = ROUNDS - 1; i >= 0; i--) {
-	  speck128_invround(&x, &y, K[i * 2 + 1]);
-   }
+    for (std::int32_t i = ROUNDS - 1; i >= 0; i--) {
+        speck128_invround(&x, &y, K[i * 2 + 1]);
+    }
 
-   pt[0] = y;
-   pt[1] = x;
+    pt[0] = y;
+    pt[1] = x;
 }
 
 static void encrypt_internal(
@@ -61,56 +60,63 @@ static void encrypt_internal(
         const std::uint64_t pt[2],  // plain text
         const std::uint64_t K[2])   // extended key
 {
-   std::uint64_t y = pt[0];
-   std::uint64_t x = pt[1];
+    std::uint64_t y = pt[0];
+    std::uint64_t x = pt[1];
 
-   for (std::int32_t i = 0; i < ROUNDS; i++) {
-	  speck128_round(&x, &y, K[i * 2 + 1]);
-   }
+    for (std::int32_t i = 0; i < ROUNDS; i++) {
+        speck128_round(&x, &y, K[i * 2 + 1]);
+    }
 
-   ct[0] = y;
-   ct[1] = x;
+    ct[0] = y;
+    ct[1] = x;
 }
 
 void extend_key(std::uint64_t kb[2 * ROUNDS], 
-	 	        const std::uint64_t K[2]) {
-	std::uint64_t i, a, b;
-	b = K[1], a = K[0];
-	for (i = 0; i < ROUNDS; i++) {
-		kb[i * 2] = b;
-		kb[i * 2 + 1] = a;
-		speck128_round(&b, &a, i);
-   }
+                const std::uint64_t K[2]) {
+                std::uint64_t i, a, b;
+    b = K[1], a = K[0];
+    for (i = 0; i < ROUNDS; i++) {
+        kb[i * 2] = b;
+        kb[i * 2 + 1] = a;
+        speck128_round(&b, &a, i);
+    }
 }
 
-void encrypt(std::uint8_t* out, const std::uint8_t* in,
+void encrypt(std::uint64_t* out, const std::uint64_t* in,
              const std::uint32_t bytes, const std::uint64_t extended_key[2 * ROUNDS]) {
-#if 0
-    dump_plain(reinterpret_cast<const std::uint64_t*>(in), extended_key);
+#if defined(CRYPT_DEBUG)
+    dump_key(extended_key);
+    dump_data("Plain", in);
 #endif
 
     std::uint32_t count = bytes / 8;
-	std::uint32_t i = 0;
-	while (i < count) {
+    std::uint32_t i = 0;
+    while (i < count) {
         encrypt_internal(reinterpret_cast<std::uint64_t*>(out) + i,
                          reinterpret_cast<const std::uint64_t*>(in) + i,
                          extended_key);
-		i += 2;
-	}
+        i += 2;
+    }
 
-#if 0
-    dump_crypt(reinterpret_cast<std::uint64_t*>(out));
+#if defined(CRYPT_DEBUG)
+    dump_data("Crypt", out);
 #endif
 }
 
-void decrypt(std::uint8_t* out, const std::uint8_t* in,
+void decrypt(std::uint64_t* out, const std::uint64_t* in,
              const std::uint32_t bytes, const std::uint64_t extended_key[2 * ROUNDS]) {
+#if defined(CRYPT_DEBUG)
+    dump_data("Crypt", in);
+#endif
+
     std::uint32_t count = bytes / 8;
-	std::uint32_t i = 0;
-	while (i < count) {
-        decrypt_internal(reinterpret_cast<std::uint64_t*>(out) + i,
-                         reinterpret_cast<const std::uint64_t*>(in) + i,
-                         extended_key);
-		i += 2;
-	}
+    std::uint32_t i = 0;
+    while (i < count) {
+        decrypt_internal(out + i, in + i, extended_key);
+        i += 2;
+    }
+
+#if defined(CRYPT_DEBUG)
+    dump_data("Plain", out);
+#endif
 }
--- bootloader/F1/Src/crypt.h
+++ bootloader/F1/Src/crypt.h
@@ -9,10 +9,10 @@ extern "C" {
 void extend_key(uint64_t kb[2 * ROUNDS], 
 	 	        const uint64_t K[2]);
 
-void encrypt(uint8_t* out, const uint8_t* in,
+void encrypt(uint64_t* out, const uint64_t* in,
              const uint32_t bytes, const uint64_t extended_key[2 * ROUNDS]);
 
-void decrypt(uint8_t* out, const uint8_t* in,
+void decrypt(uint64_t* out, const uint64_t* in,
              const uint32_t bytes, const uint64_t extended_key[2 * ROUNDS]);
 
 #ifdef __cplusplus
